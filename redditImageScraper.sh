e791c35a9e9055ee93c3aadbd55210b2d5550a39
DryOceanMirage <david.pasko.work@gmail.com>
2023-08-08 15:44:05 +1000

    URL fix

--- redditImageScraper.sh
+++ redditImageScraper.sh
@@ -1,7 +1,6 @@
-URL="https://www.reddit.com/r/{subreddit}.json?limit={limit}"
 read -p "Enter subreddit name(Lowercase): " subreddit
 read -p "Enter the limit you want for the amount of images you want to download: " limit
-IMAGES_DIR="reddit_images/$SUBREDDIT"
+IMAGES_DIR="reddit_images/$subreddit"
 mkdir -p "$IMAGES_DIR"
-cd reddit_images/$SUBREDDIT
-wget $(curl -s -H "User-agent: 'your bot 0.1'" $URL | jq '.' | grep url_overridden_by_dest |  grep -Eo '(http|https)://[^/"]+[^"]+')
+cd reddit_images/$subreddit
+wget $(curl -s -H "User-agent: 'your bot 0.1'" https://www.reddit.com/r/{$subreddit}.json?limit={$limit} | jq '.' | grep url_overridden_by_dest |  grep -Eo '(http|https)://[^/"]+[^"]+')
